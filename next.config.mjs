79c0cebecb4c0dd3467aa9e170a87a3c84921f01
rishabhma99 <87551304+rishabhma99@users.noreply.github.com>
2025-10-13 20:14:43 -0700

    -

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import("next").NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS === "true";
+const repository = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "";
+
+const assetPrefix = isGithubActions && repository ? `/${repository}` : "";
+
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
+  assetPrefix,
+  basePath: assetPrefix,
+  trailingSlash: true,
   experimental: {
     typedRoutes: true
   }
